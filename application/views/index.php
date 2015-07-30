db985225b4dfea5642c4bb0508020d6021d7ee6f
Jayson Martinez <jlmartinez631@gmail.com>
2015-07-30 17:48:35 +0800

    add registration link

--- application/views/index.php
+++ application/views/index.php
@@ -24,6 +24,9 @@
                     <li>
                         <a href="/login">Login</a>
                     </li>
+                    <li>
+                        <a href="/register">Register</a>
+                    </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
