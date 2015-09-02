d5be57950313cbc8c8cb66cf828c830228d3bfab
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-02 10:26:29 +0800

    preview image

--- application/views/admin/maps.php
+++ application/views/admin/maps.php
@@ -9,8 +9,25 @@
                     <div class="panel-body">
                         <div class="container-fluid">
                             <div class="col-md-12">
-                                <form class="" action="index.html" method="post">
-                                    <textarea name="caption" class="form-control" placeholder="Enter caption / name"></textarea>
+                                <form action="/hazzard_maps" method="post">
+                                    <?php echo $error; ?>
+                                    <div class="col-sm-4 col-sm-offset-3">
+                                        <div class="fileinput fileinput-new center-block" style="margin-left:100px;" data-provides="fileinput">
+                                            <div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 200px; height: 150px;"></div>
+                                            <div>
+                                                <span class="btn btn-default btn-file">
+                                                    <span class="fileinput-new">Select image</span>
+                                                    <span class="fileinput-exists">Change</span>
+                                                    <input type="file" name="userfile">
+                                                </span>
+                                                <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Enter caption / name" name="caption" value="">
+                                    <button type="submit" class="btn btn-primary pull-right margin-top-5px" name="button">
+                                        <i class="glyphicon glyphicon-floppy-disk"></i>&nbsp;Add
+                                    </button>
                                 </form>
                             </div>
                         </div>
@@ -20,5 +37,6 @@
         </div>
     </div>
     <?php $this->load->view('includes/footer') ?>
+    <script type="text/javascript" src="/assets/js/jasny-bootstrap.min.js"></script>
 </body>
 </html>
