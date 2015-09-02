6a3cef25577754c6c82f232efa6947c6db571b1a
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-02 08:37:22 +0800

    modal login

--- application/views/hotlines.php
+++ application/views/hotlines.php
@@ -41,5 +41,9 @@
             </div>
         </div>
     </div>
-    <?php $this->load->view('includes/footer') ?>
+    <?php
+        $this->load->view('forms/login');
+        $this->load->view('includes/footer');
+     ?>
+    <script type="text/javascript" src="/assets/js/login_register.js"></script>
 </body>
