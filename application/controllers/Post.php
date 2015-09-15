3aca82cdebf2e6e934c0c56a952f4f15f8ef036c
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-15 18:17:47 +0800

    add some commen

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -31,6 +31,7 @@ class Post extends CI_Controller
             }
             else
             {
+                //<p>You did not select a file to upload.</p>
                 $data['attach']     =  $this->upload->data('file_name');
                 $data['message']    = $this->input->post('post');
                 $data['user']       = $this->session->userdata('id');
