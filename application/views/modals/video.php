f8c9ba1ab697a5d1367890c8ec2638e2a20f926b
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-19 22:00:50 +0800

    fix error in video

--- application/views/modals/video.php
+++ application/views/modals/video.php
@@ -25,13 +25,15 @@
                                 <div class="carousel-inner">
                                     <?php
                                         $vid = $this->db->get('video')->result_array();
+                                        $ctr = 0;
                                         foreach($vid as $video)
                                         {
                                             ?>
-                                        <div class="item active">
+                                        <div class="item <?php echo $ctr == 0 ? 'active' : '' ?>">
                                             <video width="100%" height="500px" src="/assets/uploads/<?php echo $video['file'] ?>" controls preload="none" poster="posterimage.jpg" allowfullscreen=""></video>
                             			</div>
                                     <?php
+                                            $ctr++;
                                         }
                                      ?>
                                 </div>
