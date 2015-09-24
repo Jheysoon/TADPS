21dee200dded0a070e0aa10824a97e9470be35af
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-24 09:37:13 +0800

    fix video not playing in index

--- application/views/modals/video.php
+++ application/views/modals/video.php
@@ -11,41 +11,26 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12">
-                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                                <!-- Indicators -->
-                                <!-- <ol class="carousel-indicators">
-                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-                                </ol> -->
-
-                                <!-- Wrapper for slides -->
-                                <div class="carousel-inner">
+                            <div id="carousel-example-generic" class="carousel slide" style="height: 460px;">
+                        		<div class="carousel-inner">
                                     <?php
                                         $vid = $this->db->get('video')->result_array();
                                         $ctr = 0;
                                         foreach($vid as $video)
                                         {
                                             ?>
-                                        <div class="item <?php echo $ctr == 0 ? 'active' : '' ?>">
+                                        <div class="item <?php echo $ctr == 0 ? 'active':'' ?>">
                                             <video width="100%" height="500px" src="/assets/uploads/<?php echo $video['file'] ?>" controls preload="none" poster="posterimage.jpg" allowfullscreen=""></video>
                             			</div>
                                     <?php
                                             $ctr++;
                                         }
                                      ?>
-                                </div>
+                        		</div>
+                        		<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">‹</a>
+                        		<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">›</a>
+                        	</div>
 
-                                <!-- Controls -->
-                                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                    <span class="glyphicon glyphicon-chevron-left"></span>
-                                </a>
-                                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                    <span class="glyphicon glyphicon-chevron-right"></span>
-                                </a>
-                            </div>
                         </div>
                     </div>
                 </div>
