18fa3744352ec4401795f17bcf752db1c9000114
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-05 16:31:32 +0800

    change attribute in input

--- application/views/change_pass.php
+++ application/views/change_pass.php
@@ -7,11 +7,11 @@
                 <?php echo $error ?>
                 <form action="/change_password" method="post">
                     <label>Old password</label>
-                    <input type="text" class="form-control" name="old_pass" value="<?php echo set_value('old_pass') ?>">
+                    <input type="password" class="form-control" name="old_pass" value="<?php echo set_value('old_pass') ?>">
                     <label>New password</label>
-                    <input type="text" class="form-control" name="new_pass" value="<?php echo set_value('new_pass') ?>">
+                    <input type="password" class="form-control" name="new_pass" value="<?php echo set_value('new_pass') ?>">
                     <label>Confirm password</label>
-                    <input type="text" class="form-control" name="r_pass" value="<?php echo set_value('r_pass') ?>">
+                    <input type="password" class="form-control" name="r_pass" value="<?php echo set_value('r_pass') ?>">
                     <a href="<?php echo base_url() ?>" class="btn btn-default pull-left margin-top-5px">Back</a>
                     <input type="submit" class="btn btn-primary pull-right margin-top-5px" value="Update">
                 </form>
