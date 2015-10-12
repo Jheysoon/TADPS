4a63be8b0fe73df766a376e64f7d32b13a710e98
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-12 16:03:41 +0800

    add delete button and change ordering

--- application/views/user/view_prev.php
+++ application/views/user/view_prev.php
@@ -8,7 +8,7 @@
                     <div class="well well-lg">
                     <ul class="media-list">
                         <?php
-                            $a = $this->db->query('SELECT * FROM announcement ORDER BY id ASC')->result_array();
+                            $a = $this->db->query('SELECT * FROM announcement ORDER BY id DESC')->result_array();
                             foreach($a as $aa)
                             {
                                 $u = $this->db->get_where('users', array('id' => $aa['user']))->row_array();
@@ -20,9 +20,17 @@
                                 </a>
                             </div>
                             <div class="media-body">
-                                <h4 class="media-heading">Announcement</h4>
-                                <?php echo auto_typography($aa['message']) ?>
-                                <?php echo $aa['date'] ?>
+                                <h4 class="media-heading">Announcement from:<?php echo $this->session->userdata('id') == $u['id'] ? 'You' : $u['fname'].' '.$u['lname'] ?></h4>
+                                <?php
+                                    echo auto_typography($aa['message']);
+                                    echo $aa['ttime'].' '.$aa['date'];
+                                    if ($this->session->userdata('type') == 'admin') {
+                                        ?>
+                                    <br>
+                                    <a href="/delete_annouce/<?php echo $aa['id'] ?>" onclick="return confirm('Are you to delete')" class="btn btn-danger btn-sm pull-right">Delete</a>
+                                <?php
+                                    }
+                                 ?>
                             </div>
                         </li>
                       <?php } ?>
