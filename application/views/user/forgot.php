757f6b0586cb573ce5dfa5b29803bd8d7693a1d7
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-06 18:16:53 +0800

    redesign forgot password

--- application/views/user/forgot.php
+++ application/views/user/forgot.php
@@ -4,29 +4,41 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
-                <?php echo $error ?>
-                <form action="/forgot_password" method="post" style="border:2px solid #1A237E; padding:5px; border-radius:5px;">
-                    <label>Username</label>
-                    <input type="text" name="username" class="form-control" value="<?php echo set_value('username') ?>">
-                    <label>Secret Question</label>
-                    <select class="form-control" name="secret_q">
-                        <?php
-                            $i = $this->db->get('secret_q')->result_array();
-                            foreach($i as $ii)
-                            {
-                                ?>
-                            <option value="<?php echo $ii['id'] ?>" <?php echo set_select('secret_q', $ii['id']) ?>><?php echo $ii['question'] ?></option>
-                        <?php
-                            }
-                         ?>
-                    </select>
-                    <label>Answer</label>
-                    <input type="text" name="answer" class="form-control" value="<?php echo set_value('answer') ?>">
-                    <label>New Password</label>
-                    <input type="text" name="password" class="form-control" value="<?php echo set_value('password') ?>">
-                    <input type="submit" name="name" class="btn btn-primary pull-right margin-top-5px" value="Submit">
-                    <span class="clearfix"></span>
-                </form>
+
+                <div class="card" style="background-color: #FFF;">
+                    <form action="/forgot_password" method="post">
+                        <div class="card-header">
+                            <h3 class="text-center">Forgot Password</h3>
+                        </div>
+                        <div class="card-block">
+
+                            <?php echo $error ?>
+
+                            <label>Username</label>
+                            <input type="text" name="username" class="form-control" value="<?php echo set_value('username') ?>" autofocus>
+                            <label>Secret Question</label>
+                            <select class="form-control" name="secret_q">
+                                <?php
+                                    $i = $this->db->get('secret_q')->result_array();
+                                    foreach($i as $ii)
+                                    {
+                                        ?>
+                                    <option value="<?php echo $ii['id'] ?>" <?php echo set_select('secret_q', $ii['id']) ?>><?php echo $ii['question'] ?></option>
+                                <?php
+                                    }
+                                 ?>
+                            </select>
+                            <label>Answer</label>
+                            <input type="text" name="answer" class="form-control" value="<?php echo set_value('answer') ?>">
+                            <label>New Password</label>
+                            <input type="text" name="password" class="form-control" value="<?php echo set_value('password') ?>">
+                        </div>
+                        <div class="card-footer" style="background-color: #FFF;">
+                            <input type="submit" name="name" class="btn btn-primary pull-right" value="Submit">
+                            <span class="clearfix"></span>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
