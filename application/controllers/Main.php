7a8c07bb6df2820dfe6922e5eca57ef7a2cfa6f0
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-16 16:44:52 +0800

    :art:

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -176,20 +176,20 @@ class Main extends CI_Controller
     
     function hazzard()
     {
-      $this->load->view('hazzardmap');
+        $this->load->view('hazzardmap');
     }
     
     function hot()
     {
-      $this->load->view('hotlines');
+        $this->load->view('hotlines');
     }
     
     function hazzardmaps()
     {
-      $this->load->view('includes/header');
-      $this->load->view('includes/menu', array('active' =>  'maps'));
-      $this->load->view('hazzardmap');
-      $this->load->view('includes/footer');
+        $this->load->view('includes/header');
+        $this->load->view('includes/menu', array('active' =>  'maps'));
+        $this->load->view('hazzardmap');
+        $this->load->view('includes/footer');
     }
 
     function locator($lat = 11.244072, $long = 124.999341)
