c671da3ed80b7bff9539d153f0c97e766766d59d
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-30 23:27:42 +0800

    added function for confirmed posts

--- application/models/Announce.php
+++ application/models/Announce.php
@@ -4,8 +4,13 @@ class Announce extends CI_Model
 {
     function latest()
     {
-        return $this->db->query("SELECT * FROM announcement 
+        return $this->db->query("SELECT * FROM announcement
         	WHERE id = (
         		SELECT max(id) FROM announcement WHERE confirmed = 1)")->row_array();
+    }
+
+    public function confirmedPost()
+    {
+        return $this->db->query("SELECT * FROM announcement WHERE confirmed = 1 ORDER BY id ASC")->result_array();
     }
 }
