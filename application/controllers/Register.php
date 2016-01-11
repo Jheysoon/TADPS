95bc6122cb9625ae767dbc289d82275f6eb26e08
Jayson Martinez <jlmartinez631@gmail.com>
2016-01-11 09:11:39 +0800

    password length atleast 8 characters

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -105,14 +105,16 @@ class Register extends CI_Controller
         $r2='/[a-z]/';  //lowercase
         $r3='/[!@#$%&*()^,._;:-]/';  // whatever you mean by 'special char'
         $r4='/[0-9]/';  //numbers
+
+        if (strlen($candidate) < 8) return FALSE;
         
-        if(preg_match_all($r1,$candidate, $o)<1) return FALSE;
+        if (preg_match_all($r1,$candidate, $o)<1) return FALSE;
         
-        if(preg_match_all($r2,$candidate, $o)<1) return FALSE;
+        if (preg_match_all($r2,$candidate, $o)<1) return FALSE;
         
-        if(preg_match_all($r3,$candidate, $o)<1) return FALSE;
+        if (preg_match_all($r3,$candidate, $o)<1) return FALSE;
         
-        if(preg_match_all($r4,$candidate, $o)<1) return FALSE;
+        if (preg_match_all($r4,$candidate, $o)<1) return FALSE;
         
         return TRUE;
     }
