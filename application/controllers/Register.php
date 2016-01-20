7096ce5f2a6532acf7770e92de6e760a96915e55
Jayson Martinez <jlmartinez631@gmail.com>
2016-01-20 12:38:16 +0800

    use strong password helper

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -52,7 +52,7 @@ class Register extends CI_Controller
 
                 // TODO: check for a strong password
 
-                if ($this->valid_pass($password)) {
+                if (valid_pass($password)) {
 
                     if ($password == $con_pass) {
                         $config['upload_path']          = './assets/uploads/';
@@ -97,25 +97,5 @@ class Register extends CI_Controller
                 
             }
         }
-    }
-    
-    function valid_pass($candidate) 
-    {
-        $r1='/[A-Z]/';  //Uppercase
-        $r2='/[a-z]/';  //lowercase
-        $r3='/[!@#$%&*()^,._;:-]/';  // whatever you mean by 'special char'
-        $r4='/[0-9]/';  //numbers
-
-        if (strlen($candidate) < 8) return FALSE;
-        
-        if (preg_match_all($r1,$candidate, $o)<1) return FALSE;
-        
-        if (preg_match_all($r2,$candidate, $o)<1) return FALSE;
-        
-        if (preg_match_all($r3,$candidate, $o)<1) return FALSE;
-        
-        if (preg_match_all($r4,$candidate, $o)<1) return FALSE;
-        
-        return TRUE;
     }
 }
