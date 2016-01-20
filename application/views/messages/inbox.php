dc80b68190e3a173ef1c47baf8e12c65aa942878
Jayson Martinez <jlmartinez631@gmail.com>
2016-01-20 11:04:13 +0800

    :art:

--- application/views/messages/inbox.php
+++ application/views/messages/inbox.php
@@ -3,41 +3,42 @@
     <?php $this->load->view('includes/menu', array('active' => 'messages')) ?>
     <div class="container-fluid">
 
-      <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingTwo">
-                <h4 class="panel-title">Messages</h4>
-            </div>
-            <div class="panel-body">
-                <div class="container-fluid">
-                    <div class="row" style="padding:0">
-                        <div class="col-md-4" id="user_form" style="padding:0;margin:0">
-                            <?php $this->load->view('messages/users') ?>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="panel panel-default">
-                                <div class="panel-heading" role="tab" id="headingTwo">
-                                    <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span>
-                                        <span id="user_name">Please select a user</span>
-                                    </h3>
-                                </div>
-                                <div class="panel-body">
-                                    <div class="container-fluid">
-                                        <div class="row">
-                                            <div class="panel-body" id="message_body">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">Messages</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="container-fluid">
+                        <div class="row" style="padding:0">
+                            <div class="col-md-4" id="user_form" style="padding:0;margin:0">
+                                <?php $this->load->view('messages/users') ?>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="headingTwo">
+                                        <h3 class="panel-title"><span class="glyphicon glyphicon-comment"></span>
+                                            <span id="user_name">Please select a user</span>
+                                        </h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="panel-body" id="message_body">
 
-                                            </div>
-                                            <div class="col-md-12">
-                                                <form class="chat_form" action="/" method="post">
-                                                    <input type="hidden" name="user_to" value="">
-                                                    <input type="hidden" name="user_from" value="<?php echo $this->session->userdata('id') ?>">
-                                                    <textarea name="message" class="form-control"  style="height:50px;width:100%;padding-right:10px;padding-left:10px;resize:none"></textarea>
-                                                    <input type="submit" name="name" class="btn btn-info pull-right" value="Send" style="margin-top:10px">
-                                                  <!-- <div class="fileUpload btn btn-primary pull-right">
-                                                      <span class="glyphicon glyphicon-paperclip"></span>
-                                                      <input type="file" class="upload" />
-                                                  </div> -->
-                                              </form>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <form class="chat_form" action="/" method="post">
+                                                        <input type="hidden" name="user_to" value="">
+                                                        <input type="hidden" name="user_from" value="<?php echo $this->session->userdata('id') ?>">
+                                                        <textarea name="message" class="form-control"  style="height:50px;width:100%;padding-right:10px;padding-left:10px;resize:none"></textarea>
+                                                        <input type="submit" name="name" class="btn btn-info pull-right" value="Send" style="margin-top:10px">
+                                                      <!-- <div class="fileUpload btn btn-primary pull-right">
+                                                          <span class="glyphicon glyphicon-paperclip"></span>
+                                                          <input type="file" class="upload" />
+                                                      </div> -->
+                                                  </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -46,7 +47,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
                 <hr>
             </div>
         </div>
